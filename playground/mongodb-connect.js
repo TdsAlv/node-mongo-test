91e25692e059a3fb9dd1e93db867936e17f5d592
Tadas Alvikas <tadas.alvikas@yit.lt>
2018-05-16 17:30:01 +0300

    v3

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,7 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 
 // Connect to Mongodb this way
-MongoClient.connect('mongodb://localhost:27017/TodoApp', function(err,db) {
+MongoClient.connect('mongodb://localhost:27017/TodoApp', function(err, client) {
     if (err) {
         return console.log('Unable to connect to MongoDB database');
     }
@@ -9,18 +9,31 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', function(err,db) {
     const db = client.db('TodoApp');
 
     // Add some data to the TodoApp collection. You don't need to create it, just insert data into it and thats it.
-    db.collection('Todos').insertOne({
-        text: 'Something to do',
-        completed: false
-    },
-    function(err, result) {
+    // db.collection('Todos').insertOne({
+    //     text: 'Something to do',
+    //     completed: false
+    // },
+    // function(err, result) {
+    //     if (err) {
+    //         return console.log('Error inserting data!', err);
+    //     }
+    //     // If everything is ok, print the results to the terminal.
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // })
+
+    db.collection('Users').insertOne({
+        name: 'Teddy',
+        age: 81,
+        location: 'Santa Cruz'
+    }, function(err, result) {
         if (err) {
             return console.log('Error inserting data!', err);
+        } else {
+            console.log(JSON.stringify(result.ops, undefined, 2));
         }
-        // If everything is ok, print the results to the terminal.
-        console.log(JSON.stringify(result.ops, undefined, 2));
-    })
+    });
+
 
     // Close the connection
-    db.close();
+    client.close();
 });
