cc4d4abbea58fc9f5c55dd6f61d971f486d2a112
Tadas Alvikas <tadas.alvikas@yit.lt>
2018-05-16 17:17:58 +0300

    V2

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -6,6 +6,21 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', function(err,db) {
         return console.log('Unable to connect to MongoDB database');
     }
     console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // Add some data to the TodoApp collection. You don't need to create it, just insert data into it and thats it.
+    db.collection('Todos').insertOne({
+        text: 'Something to do',
+        completed: false
+    },
+    function(err, result) {
+        if (err) {
+            return console.log('Error inserting data!', err);
+        }
+        // If everything is ok, print the results to the terminal.
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    })
+
     // Close the connection
     db.close();
 });
