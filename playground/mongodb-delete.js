e3a21267f31e5729353fc0c1e8bc676ecb1e11d8
Tadas Alvikas <tadas.alvikas@yit.lt>
2018-05-17 16:58:22 +0300

    v5

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -1,19 +1,35 @@
 const MongoClient = require('mongodb').MongoClient;
 
 // Connect to Mongodb this way
-MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true} ,function(err, client) {
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true}, function(err, client) {
     if (err) {
         return console.log('Unable to connect to MongoDB database');
     }
     console.log('Connected to MongoDB server');
     const db = client.db('TodoApp');
 
+    // deleteMany
+    // db.collection('Todos').deleteMany({text: 'Walk the tog'})
+    // .then(function(result) {
+    //     console.log(result);
+    // }, function(err) {
+    //     console.log('Unable to delete', err);
+    // });
 
-    db.collection('Todos').find().count()
-    .then(function(count) {
-        console.log('Todos count: ', count);
+    // deleteOne
+    // db.collection('Todos').deleteOne({text: 'Walk the sun'})
+    // .then(function(result) {
+    //     console.log(result);
+    // }, function(err) {
+    //     console.log('Unable to delete', err);
+    // });
+
+    // findOneAndDelete
+    db.collection('Todos').findOneAndDelete({text: 'Walk the sun'})
+    .then(function(result) {
+        console.log(result);
     }, function(err) {
-        console.log('Unable to fetch', err);
+        console.log('Unable to delete', err);
     });
 
     // Close the connection
